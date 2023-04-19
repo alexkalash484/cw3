5277f11fe669e533ed823acf6c1f4881f5bae6eb
alexkalash484 <x.kalash@gmail.com>
2023-04-19 22:24:45 +0300

    creat func filter_param_data

--- cw3_utils.py
+++ cw3_utils.py
@@ -7,3 +7,16 @@ def load_operation(oper_file):
     with open(oper_file, 'r', encoding='utf-8') as f:
         operation_data = json.load(f)
     return operation_data
+
+
+def filter_param_data(data):
+    '''Функция выбора данных по параметру "исполнено"'''
+
+    new_data = []
+    for i in data:
+        if i.get('state') == 'EXECUTED':
+           new_data.append(i)
+
+    new_data = sorted(new_data, key=lambda i: i['date'], reverse=True)
+    return new_data
+
