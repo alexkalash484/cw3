93faedfe52e09d98b3aa24ee62e56716403b1bc1
alexkalash484 <x.kalash@gmail.com>
2023-04-19 22:25:23 +0300

    creat func card_amount_format

--- cw3_utils.py
+++ cw3_utils.py
@@ -20,3 +20,11 @@ def filter_param_data(data):
     new_data = sorted(new_data, key=lambda i: i['date'], reverse=True)
     return new_data
 
+
+def card_amount_format(amount):
+    '''Функция подготовки вывода номера счета/карты в требуемом формате'''
+
+    amount = amount.split(' ')
+    if amount[0] == 'Счет':
+        return f'{amount[0]} **{amount[-1][-4:]}'
+    return f'{" ".join(amount[:-1])} {amount[-1][:4]} {amount[-1][4:6]}** **** {amount[-1][-4:]}'
